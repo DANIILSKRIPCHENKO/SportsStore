fa949320690dccccf311355fced9cc57122bbb9c
”Daniil <das76@tpu.ru>
2021-11-18 07:33:27 +0700

    Add project files.

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IproductRepository repository;
+
+        public ProductController (IproductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List() => View(repository.Products);
+    }
+}
